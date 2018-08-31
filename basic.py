f2e3b901ddbd0657d5f2ca3a5639d120c9eb655e
Josh Kleinpeter <josh@kleinpeter.org>
2018-08-30 22:45:12 -0500

    Add comments.

--- basic.py
+++ basic.py
@@ -15,7 +15,10 @@ if "http_proxy" in os.environ:
 if "https_proxy" in os.environ:
     del os.environ['https_proxy']
 
+# Client is the base class which just does queries
 class Client:
+    # Init can be passed in the API info
+    # but if you have the same values in your config, that will override them
     def __init__(self, api_url=None, access_key=None, secret_key=None, project=None, config='./config.yaml'):
         with open(config, 'r') as stream:
             conf = yaml.safe_load(stream)['rancher']
@@ -28,6 +31,15 @@ class Client:
             self.headers = {}
             self.headers['Content-Type'] = 'application/json'
 
+    # This method will return what a caller may be able to do based on the
+    # initialized configuration. Right now, it simply returns the defaults
+    # eventually it will merge that with configuration for the given project
+    def capabilities(self, project_name=None):
+        return self.defaults
+
+    # constructs a URI if given some options
+    # most URIs will take /foo and /foo/{id}
+    # this helper does the right thing if you have an {id} (in the above case) or not
     def scope_uri(self, base, option=None, delim='/'):
         if option:
             base = base + delim + option
@@ -47,14 +59,6 @@ class Client:
     def projects(self, name=None):
         return self.render(self.projects_uri(name))
 
-    # Actions:
-    #   * activateservices
-    #   * cancelrollback
-    #   * cancelupgrade
-    #   * deactivateservices
-    #   * exportconfig
-    #   * finishupgrade
-    #   * rollback
     # https://rancher.com/docs/rancher/v1.5/en/api/v1/api-resources/environments/#update
     def environments(self, project_name, name=None, action=None, body=None):
         uri = self.scope_uri(self.projects_uri(project_name) + '/environments', name)
@@ -65,9 +69,20 @@ class Client:
         uri = self.scope_uri(self.projects_uri(project_name) + '/services', name)
         return self.render(uri, action, body)
 
-    def capabilities(self, project_name=None):
-        return self.defaults
-
+    # Render is the workhorse. It takes a URI and optional action or body
+    # if there is an action, a POST is made to the URI for that action
+    # if there is a body, a PUT is made to the URI with that body
+    # if there is neither, a GET is made to the URI
+    # in all cases, we return a dict of the JSON response (even on errors)
+    #
+    # Possible Actions:
+    #   * activateservices
+    #   * cancelrollback
+    #   * cancelupgrade
+    #   * deactivateservices
+    #   * exportconfig
+    #   * finishupgrade
+    #   * rollback
     def render(self, uri, action=None, body=None):
         url = self.api_url + uri
         print(url)
@@ -82,15 +97,18 @@ class Client:
 
         return json.loads(response.text)
 
+    # Helper to print out a dict payload
     def print(self, data):
         print(json.dumps(data, sort_keys=True, indent=4))
 
+# Pull possible parameters from the environment
 client = Client(
     os.getenv('API_URL'),
     os.getenv('ACCESS_KEY'),
     os.getenv('SECRET_KEY'),
     'config.yaml')
 
+# This example iterates over all projects and environments
 #for project in client.projects()['data']:
 #    project_id = project['id']
 #    print('Project: ' + project_id)
@@ -104,6 +122,15 @@ client = Client(
 #        body['environment']['FOO'] = 'BAR'
 #        client.environments(project_id, env_id, body=body)
 
+# This is an example for a sinlge project
+# it iterates over all servies and just lists their IDs
+project_id = '1a5'
+
+print('Services for ' + project_id)
+for service in client.services(project_id)['data']:
+    print(' ' + service['id'])
+
+# These are they keys we try to update
 body = {}
 body['description'] = str(datetime.datetime.utcnow())
 body['metadata'] = {}
@@ -112,17 +139,15 @@ body['scalePolicy']['min'] = 0.1
 body['scalePolicy']['max'] = 0.8
 #body['metadata']['Time'] = str(datetime.datetime.utcnow())
 
-project_id = '1a5'
 service_id = '1s37'
 
-print('Services for ' + project_id)
-for service in client.services(project_id)['data']:
-    print(' ' + service['id'])
-
+# Then we try to update the service
 print('Updating service' + service_id + ' with ' + str(body))
 response = client.services(project_id, service_id, body=body)
 
+# Print out only the keys we attempted to update
 for key in body.keys():
     print('  ' + key + ': ' + str(response[key]))
 
+# Perform an action. This one seems to 422.
 client.print(client.services(project_id, service_id, action='finishupgrade'))
