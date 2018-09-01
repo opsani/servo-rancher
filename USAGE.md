09e28792d9d11b81491c04febd89bff6049a52e0
Josh Kleinpeter <josh@kleinpeter.org>
2018-09-01 16:03:05 -0500

    Add read capabilities to adjust. Add USAGE document.

---
+++ USAGE.md
@@ -0,0 +1,149 @@
+# List all projects
+
+```
+$ ./adjust --projects
+http://rancher.kumulus.co:8080/v1/projects
+{
+    "Default": "1a5"
+}
+```
+
+# List project information
+
+```
+$ ./adjust --projects 1a5
+http://rancher.kumulus.co:8080/v1/projects
+http://rancher.kumulus.co:8080/v1/projects/1a5
+{
+    "data": {
+        "fields": {
+            "createdStackIds": [
+                1,
+                2,
+                3,
+                4
+            ],
+            "orchestration": "cattle",
+            "servicesPortRange": {
+                "endPort": 65535,
+                "startPort": 49153
+            },
+            "startedStackIds": [
+                1,
+                2,
+                3,
+                4
+            ]
+        }
+    },
+    "id": "1a5",
+    "name": "Default"
+}
+```
+
+# List all services for the configured project
+
+```
+$ ./adjust --services
+http://rancher.kumulus.co:8080/v1/projects
+http://rancher.kumulus.co:8080/v1/projects/1a5/services
+{
+    "back": "1s9",
+    "cni-driver": "1s2",
+    "front": "1s8",
+    "healthcheck": "1s3",
+    "http-slb": "1s11",
+    "ipsec": "1s5",
+    "metadata": "1s6",
+    "network-manager": "1s1",
+    "scheduler": "1s4",
+    "secrets-driver": "1s7"
+}
+```
+
+# List all instances for a given service
+
+```
+$ ./adjust --services front
+http://rancher.kumulus.co:8080/v1/projects
+http://rancher.kumulus.co:8080/v1/projects/1a5/services
+http://rancher.kumulus.co:8080/v1/projects/1a5/services/1s8
+{
+    "id": "1s8",
+    "instanceIds": [
+        "1i30"
+    ],
+    "name": "front"
+}
+```
+
+# List instance information
+
+```
+$ ./adjust --instances front 1i30
+http://rancher.kumulus.co:8080/v1/projects
+http://rancher.kumulus.co:8080/v1/projects/1a5/services
+http://rancher.kumulus.co:8080/v1/projects/1a5/services/1s8/instances/front
+{
+    "compose": {
+        "cpu": {
+            "min": 500
+        }
+    },
+    "environment": {
+        "MEMORY": "2048M"
+    }
+}
+```
+
+# List default capabilities
+
+```
+$ ./adjust --capabilities
+{
+    "compose": {
+        "cpu": {
+            "min": 500
+        }
+    },
+    "environment": {
+        "MEMORY": {
+            "max": 16384,
+            "min": 4096,
+            "step": 64,
+            "template": "MEMORY: '{{parameter}}M'"
+        }
+    }
+}
+```
+
+# List capabilities for a service
+
+```
+$ ./adjust --capabilities front
+{
+    "compose": {
+        "cpu": {
+            "min": 500
+        }
+    },
+    "environment": {
+        "GC": {
+            "template": "GC: '-XX:+UsePArallelGC -XX:ParallelGCThreads={{parameter}}'"
+        },
+        "MEMORY": {
+            "max": 16384,
+            "min": 4096,
+            "step": 64,
+            "template": "MEMORY: '{{parameter}}M'"
+        }
+    }
+}
+```
+
+# List capabilities for a service which is excluded
+
+```
+$ ./adjust --capabilities http-slb
+{}
+```
--- client.py
+++ client.py
@@ -19,23 +19,52 @@ if "https_proxy" in os.environ:
 class Client:
     # Init can be passed in the API info
     # but if you have the same values in your config, that will override them
-    def __init__(self, api_url=None, access_key=None, secret_key=None, project=None, config='./config.yaml'):
-        with open(config, 'r') as stream:
-            conf = yaml.safe_load(stream)['rancher']
-            self.api_url = conf.get('api_url', api_url)
-            self.access_key = conf.get('api_key', access_key)
-            self.secret_key = conf.get('secret_key', secret_key)
-            self.project = conf.get('project', project)
-            self.stack = conf.get('stack')
-            self.defaults = conf.get('default')
-            self.headers = {}
-            self.headers['Content-Type'] = 'application/json'
+    def __init__(self, config=None):
+        self.config = config
+        self.headers = {}
+        self.headers['Content-Type'] = 'application/json'
+        self.name_mappings = {}
+
+    def names_to_ids(self, response):
+        hash = {}
+        for datum in response['data']:
+            hash[datum['name']] = datum['id']
+        return hash
+
+    def name_to_id(self, name, key, function):
+        if name == None:
+            return None
+        if self.name_mappings.get(key) == None:
+            self.name_mappings[key] = self.names_to_ids(function())
+
+        return self.name_mappings[key].get(name, name)
+
+    def project_id(self, name):
+        return self.name_to_id(name, 'project', lambda: self.projects())
+
+    def service_id(self, name):
+        return self.name_to_id(name, 'service', lambda: self.services())
 
     # This method will return what a caller may be able to do based on the
-    # initialized configuration. Right now, it simply returns the defaults
-    # eventually it will merge that with configuration for the given project
-    def capabilities(self, project_name=None):
-        return self.defaults
+    # initialized configuration. It only supports service overrides.
+    def capabilities(self, service_name=None):
+        merged = self.merge(
+            self.config.defaults.copy(),
+            self.config.services.get(service_name, {}))
+        if merged.get('exclude', None) != None or service_name in self.config.excluded:
+            return {}
+        return merged
+
+    def merge(self, source, destination):
+        for key, value in source.items():
+            if isinstance(value, dict):
+                # get node or create one
+                node = destination.setdefault(key, {})
+                self.merge(value, node)
+            else:
+                destination[key] = value
+
+        return destination
 
     # constructs a URI if given some options
     # most URIs will take /foo and /foo/{id}
@@ -53,20 +82,29 @@ class Client:
         uri = self.scope_uri('/accounts', name, '?name=')
         return self.render(uri)
 
-    def projects_uri(self, name=None):
-        return self.scope_uri('/projects', name)
+    def projects_uri(self, name=None, default=False):
+        if default and name == None:
+            name = self.config.project
+        return self.scope_uri('/projects', self.project_id(name))
 
     def projects(self, name=None):
         return self.render(self.projects_uri(name))
 
     # https://rancher.com/docs/rancher/v1.5/en/api/v1/api-resources/environments/#update
-    def environments(self, project_name, name=None, action=None, body=None):
-        uri = self.scope_uri(self.projects_uri(project_name) + '/environments', name)
+    def environments(self, project_name=None, name=None, action=None, body=None):
+        uri = self.scope_uri(self.projects_uri(project_name, True) + '/environments', name)
         return self.render(uri, action, body)
 
+    def services_uri(self, project_name=None, name=None):
+        return self.scope_uri(self.projects_uri(project_name, True) + '/services', self.service_id(name))
+
     # https://rancher.com/docs/rancher/v1.5/en/api/v1/api-resources/service/#update
-    def services(self, project_name, name=None, action=None, body=None):
-        uri = self.scope_uri(self.projects_uri(project_name) + '/services', name)
+    def services(self, project_name=None, name=None, action=None, body=None):
+        uri = self.services_uri(project_name, name)
+        return self.render(uri, action, body)
+
+    def instances(self, project_name=None, service_name=None, name=None, action=None, body=None):
+        uri = self.scope_uri(self.services_uri(project_name, service_name) + '/instances', name)
         return self.render(uri, action, body)
 
     # Render is the workhorse. It takes a URI and optional action or body
@@ -84,18 +122,25 @@ class Client:
     #   * finishupgrade
     #   * rollback
     def render(self, uri, action=None, body=None):
-        url = self.api_url + uri
-        print(url)
+        url = self.config.api_url + uri
+        print(url, file=sys.stderr) # DEBUG URL info to stderr
+
+        auth = (self.config.access_key, self.config.secret_key)
 
         if action:
             url = url + '?action=' + action
-            response = requests.post(url, auth=(self.access_key, self.secret_key))
+            response = requests.post(url, auth=auth)
         elif body:
-            response = requests.put(url, data=body, auth=(self.access_key, self.secret_key))
+            response = requests.put(url, data=body, auth=auth)
         else:
-            response = requests.get(url, auth=(self.access_key, self.secret_key))
+            response = requests.get(url, auth=auth)
 
-        return json.loads(response.text)
+        data = json.loads(response.text)
+        if response.status_code == requests.codes.ok:
+            return data
+        else:
+            self.print(data)
+            sys.exit(3)
 
     # Helper to print out a dict payload
     def print(self, data):
@@ -107,6 +152,24 @@ class Config:
         self.secret_key = self.read_key('/var/run/secret_key', 'SECRET_KEY')
         self.api_url = os.getenv('API_URL')
         self.config_file = os.getenv('CONFIG', 'config.yaml')
+        try:
+            with open(self.config_file, 'r') as stream:
+                conf = yaml.safe_load(stream)['rancher']
+                self.api_url = conf.get('api_url', self.api_url)
+                self.access_key = conf.get('api_key', self.access_key)
+                self.secret_key = conf.get('secret_key', self.secret_key)
+                self.project = conf.get('project')
+                self.stack = conf.get('stack')
+                self.defaults = conf.get('default', {})
+                self.services = conf.get('service', {})
+                self.excluded = conf.get('excluded', [])
+        except IOError as e:
+            if e.errno == errno.ENOENT:
+                return {} # only if 'file not found'
+            raise ConfigError("cannot read configuration from {}:{}".format(config, e.strerror))
+        except yaml.error.YAMLError as e:
+            raise ConfigError("syntax error in {}: {}".format(config, str(e)))
+
 
     def read_key(self, filename, default_env=None):
         try:
@@ -116,41 +179,38 @@ class Config:
             return os.getenv(default_env)
 
 
-config = Config()
-# Pull possible parameters from the environment
-client = Client(
-    config.api_url,
-    config.access_key,
-    config.secret_key,
-    config.config_file)
+if __name__ == "__main__":
+    config = Config()
+    # Pull possible parameters from the environment
+    client = Client(config)
 
-# This is an example for a sinlge project
-# it iterates over all services and just lists their IDs
-client.print(client.projects())
-project_id = client.projects()['data'][0]['id']
+    # This is an example for a single project
+    # it iterates over all services and just lists their IDs
+    client.print(client.projects())
+    project_id = client.projects()['data'][0]['id']
 
-print('Services for ' + project_id)
-for service in client.services(project_id)['data']:
-    print(' ' + service['id'])
+    print('Services for ' + project_id)
+    for service in client.services(project_id)['data']:
+        print(' ' + service['id'])
 
-# These are they keys we try to update
-body = {}
-body['description'] = str(datetime.datetime.utcnow())
-body['metadata'] = {}
-body['scalePolicy'] = {}
-body['scalePolicy']['min'] = 0.1
-body['scalePolicy']['max'] = 0.8
-#body['metadata']['Time'] = str(datetime.datetime.utcnow())
+    # These are they keys we try to update
+    body = {}
+    body['description'] = str(datetime.datetime.utcnow())
+    body['metadata'] = {}
+    body['scalePolicy'] = {}
+    body['scalePolicy']['min'] = 0.1
+    body['scalePolicy']['max'] = 0.8
+    #body['metadata']['Time'] = str(datetime.datetime.utcnow())
 
-service_id = client.services(project_id)['data'][0]['id']
+    service_id = client.services(project_id)['data'][0]['id']
 
-# Then we try to update the service
-print('Updating service' + service_id + ' with ' + str(body))
-response = client.services(project_id, service_id, body=body)
+    # Then we try to update the service
+    print('Updating service' + service_id + ' with ' + str(body))
+    response = client.services(project_id, service_id, body=body)
 
-# Print out only the keys we attempted to update
-for key in body.keys():
-    print('  ' + key + ': ' + str(response[key]))
+    # Print out only the keys we attempted to update
+    for key in body.keys():
+        print('  ' + key + ': ' + str(response[key]))
 
-# Perform an action. This one seems to 422.
-client.print(client.services(project_id, service_id, action='finishupgrade'))
+    # Perform an action. This one seems to 422.
+    client.print(client.services(project_id, service_id, action='finishupgrade'))
